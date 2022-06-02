85084796d9a5fc71cb4b382a9357919c6c4456c9
v715 <vgopala4@jhu.edu>
2022-06-02 17:08:33 -0400

    Add a direct sampling function

--- src/DRRs.jl
+++ src/DRRs.jl
@@ -8,7 +8,10 @@ export Camera, Detector, make_plane, get_rays, trace
 include("camera.jl")
 
 # Sampling
-export make_coordinate_matrix, make_inverse_coordinate_matrix, interpolate
+export make_coordinate_matrix, make_inverse_coordinate_matrix, trilinear
 include("sampling/trilinear.jl")
+
+export sample
+include("sampling/sampler.jl")
 
 end
---
+++ src/sampling/sampler.jl
@@ -0,0 +1,15 @@
+function sample(x::Float64, y::Float64, z::Float64; volume, xs, ys, zs)
+    # Find the indices of the lower left corner of the cube we're inside of
+    xidx = findlast(xs .<= x)
+    yidx = findlast(ys .<= y)
+    zidx = findlast(zs .<= z)
+
+    if any(map(x -> isnothing(x), (xidx, yidx, zidx)))
+        return -1024.0
+    end
+    if xidx == length(xs) || yidx == length(ys) || zidx == length(zs)
+        return -1024.0
+    end
+
+    return volume[xidx, yidx, zidx]
+end
