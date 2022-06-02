f56afb4c5da5df049b07d2b1cc60fb8b5e5c7f05
v715 <vgopala4@jhu.edu>
2022-06-02 16:10:09 -0400

    Add function to make detector plane

--- src/camera.jl
+++ src/camera.jl
@@ -1,5 +1,7 @@
 using StaticArrays
 
+import Base: product
+
 """
     Camera
 """
@@ -18,3 +20,14 @@ mutable struct Detector
     Δx::Float64
     Δy::Float64
 end
+
+function make_plane(detector::Detector)
+    d = d = detector.origin' * detector.normal
+    xs = (-detector.height÷2:1:detector.height÷2) * detector.Δx
+    ys = (-detector.width÷2:1:detector.width÷2) * detector.Δy
+    xys = product(xs, ys)
+    zs = [findz(xy..., detector.normal..., d) for xy in xys]
+    return [SVector(xy..., z) for (xy, z) in zip(xys, zs)]
+end
+
+findz(x, y, a, b, c, d) = (d - a * x - b * y) / c
