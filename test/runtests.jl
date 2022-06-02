e61a7a21b0ee715e4d60e781970d130706f0d316
v715 <vgopala4@jhu.edu>
2022-06-02 16:10:12 -0400

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,4 +6,8 @@ using Test
     volume, ΔX, ΔY, ΔZ = read_dicom("../data/cxr")
     @test size(volume) == (512, 512, 133)
     @show ΔX, ΔY, ΔZ
+
+    @info "Testing camera submodule"
+    detector = Detector([2, 1, 0.2], [-1, 0, 1], 101, 101, 0.4, 0.4)
+    @test size(make_plane(detector)) == (101, 101)
 end
