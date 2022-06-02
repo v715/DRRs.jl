36c32e8dc772d945e90d6b07d7dcb2dde8163665
v715 <vgopala4@jhu.edu>
2022-06-02 17:38:25 -0400

    Change detector origin to center

--- src/camera.jl
+++ src/camera.jl
@@ -14,7 +14,7 @@ end
     Detector Plane
 """
 mutable struct Detector
-    origin::SVector{3,Float64}
+    center::SVector{3,Float64}
     normal::SVector{3,Float64}
     height::Int64
     width::Int64
@@ -23,7 +23,7 @@ mutable struct Detector
 end
 
 function make_plane(detector::Detector)
-    d = d = detector.origin' * detector.normal
+    d = d = detector.center' * detector.normal
     xs = (-detector.height÷2:1:detector.height÷2) * detector.Δx
     ys = (-detector.width÷2:1:detector.width÷2) * detector.Δy
     xys = product(xs, ys)
